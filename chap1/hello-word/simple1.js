928a04a3f58e2255a308c1d39553e124a0263769
Minjeong Kim <h_hoh@naver.com>
2021-09-27 22:52:48 +0900

    바벨 - 1

--- chap1/hello-word/simple1.js
+++ chap1/hello-word/simple1.js
@@ -8,9 +8,30 @@ function LikeButton() {
   );
 }
 
-const domContainer1 = document.getElementById('root1');
-ReactDOM.render(React.createElement(LikeButton), domContainer1)
-const domContainer2 = document.getElementById('root2');
-ReactDOM.render(React.createElement(LikeButton), domContainer2)
-const domContainer3 = document.getElementById('root3');
-ReactDOM.render(React.createElement(LikeButton), domContainer3)
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(LikeButton),
+    React.createElement(
+      'div',
+      { style: { marginTop: 20}},
+      React.createElement('span', null, '현재 카운트: '),
+      React.createElement('span', { style: { marginRight: 10 }}, count),
+      React.createElement(
+        'button',
+        { onClick: () => setCount(count + 1)},
+        '증가'
+      ),
+      React.createElement(
+        'button',
+        { onClick: () => setCount(count - 1)},
+        '감소',
+      )
+    )
+  )
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(Container), domContainer);
